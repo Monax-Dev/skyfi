9d5dcde7f1b3e27bd6e158e16da6fd86a8e7c89f
Abdoulaye SARRE <abdoulaye.sarre0411@gmail.com>
2024-07-29 20:23:06 +0000

    gestion compagni 90%

---
+++ app/(root)/dashboard/client/page.tsx
@@ -0,0 +1,12 @@
+import  NavBar  from '@/components/us/navBar'
+import DashboardLayout  from '../layout'
+
+import ListFactures from '@/components/us/listFactures'
+export default function Facture() {
+  return (
+    <>
+      <NavBar/>
+      <ListFactures/>
+    </>
+  )
+}
